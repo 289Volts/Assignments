0d6a2f3eef5453ee87a156809c624431f4ba7713
Roland Joshua <289volts@gmail.com>
2022-10-27 15:31:15 +0100

    Add files via upload

---
+++ ZURI-WEEK3-TASK/script.js
@@ -0,0 +1,12 @@
+const open = document.getElementById('menu');
+const nav = document.getElementById('nav');
+
+function toggle() 
+{
+    nav.classList.add('show');
+}
+
+
+function hide() {
+    nav.classList.remove("show");
+}
